b74a21a9eedac7b0321eeabae8138ba4160d87c4
geocoug <geocoug@gmail.com>
2022-04-15 14:16:39 -0700

    best practice reformatting

--- create_repo.py
+++ create_repo.py
@@ -1,109 +1,112 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import json
+import logging
+import os
+
 import requests
 
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-# Read a local file containing user authentication token
-def GetToken():
+base_url = "https://api.github.com"
+
+
+def GetAuth() -> tuple:
+    """Read a local file containing username and authentication token"""
     # Name of file storing GitHub token
-    token_file = "/Users/cgrant/OneDrive/GitHub/github.txt"
+    auth_file = os.path.join(
+        os.path.dirname(os.path.dirname(os.getcwd())), "github.txt"
+    )
     try:
-        with open(token_file, 'r') as f:
-            token = f.readlines()[0]
-    except:
-        print('No token found.')
-        sys.exit()
-    return token
+        with open(auth_file, "r") as f:
+            auth = f.readlines()
+    except FileExistsError as e:
+        raise e
+    try:
+        token = auth[0].strip()
+        username = auth[1].strip()
+    except Exception as e:
+        raise e
+    return username, token
 
-# --------------------------------------------------------------
 
-# Check the status of a response for any errors and
-#   print them to the console if any occur.
-def ResponseStatus(response):
-    if 'errors' in json.loads(response.text):
-        errors = ''
-        for e in json.loads(response.text)['errors']:
-            if errors == '':
-                errors = "{}".format(e['message'])
+def ResponseStatus(response: requests.models.Response) -> None:
+    """Check the status of a response for any errors and
+    print them to the console if any occur."""
+    if "errors" in json.loads(response.text):
+        errors = ""
+        for e in json.loads(response.text)["errors"]:
+            if errors == "":
+                errors = f"{e['message']}"
             else:
-                errors = errors + ". {}".format(e['message'])
-        print("[{}] {}\nError(s): {}".format(response.status_code, json.loads(response.text)['message'], errors))
-        sys.exit()
+                errors = errors + f". {e['message']}"
+        raise Exception(
+            f"""[{response.status_code}] {json.loads(response.text)['message']}\n
+            Error(s): {errors}"""
+        )
 
-# --------------------------------------------------------------
 
-# Send a POST request and check the response for errors
-def CreateRepo(url, data, headers):
-    response = requests.post(url, data = json.dumps(data), headers = headers)
+def CreateRepo(url: str, data: dict, headers: dict) -> None:
+    """Send a POST request and check the response for errors"""
+    response = requests.post(url, data=json.dumps(data), headers=headers)
     ResponseStatus(response)
-    print('Successfully created!')
+    logging.info("Successfully created!")
     response_json = json.loads(response.text)
-    print('  > {}'.format(response_json['clone_url']))
+    logging.info(f"  > {response_json['clone_url']}")
 
-# --------------------------------------------------------------
 
-# Send a DELETE request
-def DelRepo(url, headers):
+def DeleteRepo(url: str, headers: dict) -> None:
+    """Send a DELETE request"""
     try:
-        requests.delete(url, headers = headers)
-    except requests.exceptions.RequestException as e:
-        print('Failed to delete <{}>'.format(url))
-        print(e)
+        requests.delete(url, headers=headers)
+    except requests.exceptions.RequestException:
+        raise requests.exceptions.RequestException(f"Failed to delete <{url}>")
 
-# --------------------------------------------------------------
 
-# Ask the user if they want to delete the repository 
-#   that was just created
-def RevertChanges():
+def RevertChanges(username: str, token: str, repo_name: str) -> None:
+    """Ask the user if they want to delete the repository
+    that was created."""
     while True:
         revert = input("Do you want to keep these changes [Y/n]: ").strip()
-        if not (revert.upper() == 'Y' or revert.upper() == 'N'):
-            print('Error: please enter [Y] or [n]')
+        if not (revert.upper() == "Y" or revert.upper() == "N"):
+            logging.warning("Error: please enter [Y] or [n]")
         else:
-            if revert.upper() == 'N':
-                DelRepo(
-                    base_url + "/repos/{}/{}".format(username, repo_name), 
-                    {'Authorization': "token {}".format(token)}
+            if revert.upper() == "N":
+                DeleteRepo(
+                    f"{base_url}/repos/{username}/{repo_name}",
+                    {"Authorization": f"token {token}"},
                 )
-                print('Repository deleted.')
+                logging.info("Repository deleted.")
             break
 
-# --------------------------------------------------------------
 
-if __name__ == "__main__":
-    print('-' * 48)
-    print('Create a GitHub repository from the command line')
-    print('-' * 48)
+def main() -> None:
+    """Main function"""
+    username, token = GetAuth()
 
-    username = 'geocoug'
-    token = GetToken()
-    base_url = 'https://api.github.com'
-    
-    repo_name = input('Enter a repository name: ').strip()
+    repo_name = input("Enter a repository name: ").strip()
     while True:
-        private = input('Private repository [Y/n]: ').strip()
-        if not (private.upper() == 'Y' or private.upper() == 'N'):
-            print('Error: please enter [Y] or [n]')
+        private = input("Private repository [Y/n]: ").strip()
+        if not (private.upper() == "Y" or private.upper() == "N"):
+            print("Error: please enter [Y] or [n]")
         else:
             break
-    if private.upper() == 'Y':
+    if private.upper() == "Y":
         private = True
     else:
         private = False
-
-    # Ask for repo description
-    repo_desc = input('Description (optional): ').strip()
-
-    # Create a repo
+    repo_desc = input("Description (optional): ").strip()
     CreateRepo(
-        base_url + '/user/repos', 
-        {"name": repo_name, "private": private, "description": repo_desc}, 
-        {'Authorization': "token {}".format(token)}
+        base_url + "/user/repos",
+        {"name": repo_name, "private": private, "description": repo_desc},
+        {"Authorization": "token {}".format(token)},
     )
-    # Ask user if they want to keep these changes
-    RevertChanges()
+    RevertChanges(username, token, repo_name)
 
-    print('Done.')
+
+if __name__ == "__main__":
+    logging.info("-" * 48)
+    logging.info("Create a GitHub repository from the command line")
+    logging.info("-" * 48)
+    main()
+    logging.info("Complete.")
